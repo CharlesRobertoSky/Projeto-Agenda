a0053954a4cdd7c7f8824b5a839c82ce766175e5
Charles <charlesdelimaroberto@gmail.com>
2023-08-07 10:53:04 -0300

    att 7 ago

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variavel local'
-
+  res.locals.errors = req.flash('errors')
+  res.locals.success = req.flash('success')
+  
   next()
 }
 exports.outroMiddleware = (req, res, next) => {
--- src/models/HomeModels.js
+++ src/models/HomeModels.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const HomeSchema = new mongoose.Schema({
-  titulo:{type: String, required:true},
+  titulo: { type: String, required: true },
   descricao: String
-});
+})
 
-const HomeModel = mongoose.model('Home', HomeSchema);
+const HomeModel = mongoose.model('Home', HomeSchema)
 
-export default class Home {
-  constructor()
-}
+class Home {}
+
+module.exports = Home
